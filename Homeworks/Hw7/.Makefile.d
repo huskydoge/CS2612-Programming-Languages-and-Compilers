65c4107f652a661d1cb105e50ebf7d519a76910e
huskydoge <hbh001098hbh@sjtu.edu.cn>
2023-10-11 19:30:31 +0800

    hw7-raw

---
+++ Homeworks/Hw7/.Makefile.d
@@ -0,0 +1,9 @@
+Assignment1010.vo Assignment1010.glob Assignment1010.v.beautified Assignment1010.required_vo: Assignment1010.v SetsClass/SetsClass.vo SyntaxInCoq.vo DenotationalSemantics.vo
+Assignment1010.vio: Assignment1010.v SetsClass/SetsClass.vio SyntaxInCoq.vio DenotationalSemantics.vio
+Assignment1010.vos Assignment1010.vok Assignment1010.required_vos: Assignment1010.v SetsClass/SetsClass.vos SyntaxInCoq.vos DenotationalSemantics.vos
+DenotationalSemantics.vo DenotationalSemantics.glob DenotationalSemantics.v.beautified DenotationalSemantics.required_vo: DenotationalSemantics.v SetsClass/SetsClass.vo SyntaxInCoq.vo
+DenotationalSemantics.vio: DenotationalSemantics.v SetsClass/SetsClass.vio SyntaxInCoq.vio
+DenotationalSemantics.vos DenotationalSemantics.vok DenotationalSemantics.required_vos: DenotationalSemantics.v SetsClass/SetsClass.vos SyntaxInCoq.vos
+SyntaxInCoq.vo SyntaxInCoq.glob SyntaxInCoq.v.beautified SyntaxInCoq.required_vo: SyntaxInCoq.v 
+SyntaxInCoq.vio: SyntaxInCoq.v 
+SyntaxInCoq.vos SyntaxInCoq.vok SyntaxInCoq.required_vos: SyntaxInCoq.v 
--- README.md
+++ README.md
@@ -19,4 +19,7 @@ SJTU |   CS 2612, Programming Languages and Compilers, Fall 2023
 * https://regexr.com/
 
 ## Common Problem
+
+* Project Complie: `coq_makefile -f _CoqProject *.v -o Makefile`
+
 * Incorrect display of Chinese characters (Displayed in Unicode), you could transfer it to normal Chinese Character by some common tools like: http://www.msxindl.com/tools/unicode16.asp
