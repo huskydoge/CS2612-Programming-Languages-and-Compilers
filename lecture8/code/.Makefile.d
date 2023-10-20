ce17b977760584108939f8b9f74fd1e77fa3a2ff
huskydoge <hbh001098hbh@sjtu.edu.cn>
2023-10-20 22:01:02 +0800

    modified hw1017

---
+++ lecture8/code/.Makefile.d
@@ -0,0 +1,6 @@
+DenotationalSemantics.vo DenotationalSemantics.glob DenotationalSemantics.v.beautified DenotationalSemantics.required_vo: DenotationalSemantics.v sets/SetsClass.vo pl/SyntaxInCoq.vo
+DenotationalSemantics.vio: DenotationalSemantics.v sets/SetsClass.vio pl/SyntaxInCoq.vio
+DenotationalSemantics.vos DenotationalSemantics.vok DenotationalSemantics.required_vos: DenotationalSemantics.v sets/SetsClass.vos pl/SyntaxInCoq.vos
+logic.vo logic.glob logic.v.beautified logic.required_vo: logic.v pl/CoqIntro.vo pl/InductiveType.vo
+logic.vio: logic.v pl/CoqIntro.vio pl/InductiveType.vio
+logic.vos logic.vok logic.required_vos: logic.v pl/CoqIntro.vos pl/InductiveType.vos
